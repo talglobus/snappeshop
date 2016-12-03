a981a52aca70d6a1b0470e2ac88aa3bf5d5de1fb
Spencer Hayes <hayess@bu.edu>
2016-12-03 15:22:48 -0500

    Basic RabbitMQ Implementation

--- python/WatsonLogic.py
+++ python/WatsonLogic.py
@@ -1,15 +1,33 @@
 from watson_developer_cloud import VisualRecognitionV3
+import pika
 
-visual_recognition = VisualRecognitionV3('2016-05-20', api_key='0d83972e03cb13caa44c780ae1b20d8ed48779b9')
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
 
-file = None 
-raw = visual_recognition.classify(file)
+channel.queue_declare(queue='queue')
+
+def callback(ch, method, properties, body):
+    process(body)
+    
+channel.basic_consume(callback,
+                      queue='queue',
+                      no_ack=True)
+
+def process(x):
+    visual_recognition = VisualRecognitionV3('2016-05-20', api_key='0d83972e03cb13caa44c780ae1b20d8ed48779b9')
+
+    file = x 
+    URL = "www.snappeshop.com/retrieve/"
+    raw = visual_recognition.classify(URL + file + ".png")
 #Might Not Be a Good Idea
 #if (raw['images'][0]['classifiers'][0]['classes'][1]['class']):
 #    append = raw['images'][0]['classifiers'][0]['classes'][0]['class']
 #    keyword = raw['images'][0]['classifiers'][0]['classes'][1]['class'] + " " + append
 #else:
 #    keyword = raw['images'][0]['classifiers'][0]['classes'][0]['class']
-keyword = raw['images'][0]['classifiers'][0]['classes'][0]['class']
+    keyword = raw['images'][0]['classifiers'][0]['classes'][0]['class']
+    return keyword
     
-
+print("Working")
+channel.start_consuming()
