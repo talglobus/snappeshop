b77d2bb11a53c0ac1918321b979a542291f9da33
Spencer Hayes <hayess@bu.edu>
2016-12-03 14:03:46 -0500

    Keyword Update

--- python/WatsonLogic.py
+++ python/WatsonLogic.py
@@ -1,14 +1,12 @@
-import json
-from os.path import join, dirname
-from os import environ
 from watson_developer_cloud import VisualRecognitionV3
 
 visual_recognition = VisualRecognitionV3('2016-05-20', api_key='0d83972e03cb13caa44c780ae1b20d8ed48779b9')
 
-raw = json.dumps(visual_recognition.classify(images_url="http://molotilo.com/wp-content/uploads/2016/07/black-shoes.jpg"), indent=2)
+raw = visual_recognition.classify(images_url="http://molotilo.com/wp-content/uploads/2016/07/black-shoes.jpg")
+#Might Not Be a Good Idea
+if (raw['images'][0]['classifiers'][0]['classes'][1]['class']):
+    append = raw['images'][0]['classifiers'][0]['classes'][0]['class']
+    keyword = raw['images'][0]['classifiers'][0]['classes'][1]['class'] + " " + append
+else:
+    keyword = raw['images'][0]['classifiers'][0]['classes'][0]['class']
 
-with open('data.txt', 'w') as outfile:
-    json.dump(raw, outfile)
-    
-keyword = outfile["images"]["classifiers"]["classes"]
-print(keyword)
