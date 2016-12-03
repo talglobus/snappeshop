4ef71a9e7f1038d89eaea111877aa8d550212363
talglobus <talglobus@gmail.com>
2016-12-03 13:25:53 -0500

    Add search.js to run search on provided dataURI and feed it to python with python.js, except now we're going to used shared work queues

---
+++ web/search.js
@@ -0,0 +1,16 @@
+var crypto = require('crypto');
+
+function randomID(len) {
+    return crypto.randomBytes(Math.ceil(len/2))
+        .toString('hex') // convert to hexadecimal format
+        .slice(0,len);   // return required number of characters
+}
+
+module.exports = (dataURI) => {
+	dataURI = "data:image/png;base64," + dataURI;	// Not sure if this format is correct
+	let filePath = "./images/" + randomID(24) + ".png";
+	imageDataURI.outputFile(dataURI, filePath)
+    .then(res => {
+    	console.log(res)
+    });
+}
