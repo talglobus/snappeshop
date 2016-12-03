c36e26aaa96a37b69f0b146213855b3bd5da9ff8
Spencer Hayes <hayess@bu.edu>
2016-12-03 14:17:03 -0500

    Update

--- python/WatsonLogic.py
+++ python/WatsonLogic.py
@@ -2,11 +2,14 @@ from watson_developer_cloud import VisualRecognitionV3
 
 visual_recognition = VisualRecognitionV3('2016-05-20', api_key='0d83972e03cb13caa44c780ae1b20d8ed48779b9')
 
-raw = visual_recognition.classify(images_url="http://molotilo.com/wp-content/uploads/2016/07/black-shoes.jpg")
+file = None 
+raw = visual_recognition.classify(file)
 #Might Not Be a Good Idea
-if (raw['images'][0]['classifiers'][0]['classes'][1]['class']):
-    append = raw['images'][0]['classifiers'][0]['classes'][0]['class']
-    keyword = raw['images'][0]['classifiers'][0]['classes'][1]['class'] + " " + append
-else:
-    keyword = raw['images'][0]['classifiers'][0]['classes'][0]['class']
+#if (raw['images'][0]['classifiers'][0]['classes'][1]['class']):
+#    append = raw['images'][0]['classifiers'][0]['classes'][0]['class']
+#    keyword = raw['images'][0]['classifiers'][0]['classes'][1]['class'] + " " + append
+#else:
+#    keyword = raw['images'][0]['classifiers'][0]['classes'][0]['class']
+keyword = raw['images'][0]['classifiers'][0]['classes'][0]['class']
+    
 
